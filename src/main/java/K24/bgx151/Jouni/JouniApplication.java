1c084e6591a66d4ad3e70a1cd8a878f49cd23a51
jkaitasalo <jouni.kaitasalo@hotmail.com>
2024-01-17 20:00:38 +0200

    second

---
+++ src/main/java/K24/bgx151/Jouni/JouniApplication.java
@@ -0,0 +1,13 @@
+package K24.bgx151.Jouni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JouniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JouniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/K24/bgx151/Jouni/JouniApplicationTests.java
@@ -0,0 +1,13 @@
+package K24.bgx151.Jouni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JouniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
